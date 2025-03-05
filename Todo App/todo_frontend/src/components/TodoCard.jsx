2d5e2e34a7b6a120fda90756e51e09af3b38a346
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2025-03-05 15:33:45 +0500

    Started frontend in React JS

---
+++ Todo App/todo_frontend/src/components/TodoCard.jsx
@@ -0,0 +1,12 @@
+import './Style.css'
+
+function TodoCart() {
+    return (
+        <div className="todo-card">
+            <input type="radio" />
+            <h6>This is todo card</h6>
+        </div>
+    )
+}
+
+export default TodoCart
--- Todo App/todo_list.py
+++ Todo App/todo_list.py
@@ -1,11 +1,16 @@
+import json
+import os
+
 name = input("Enter Your Name: ")
 print("Welcome", name, "\n")
-file = open("todo_list.txt", "r")
-content = file.read()
-content_found = [x.split(",") for x in content]
-print(content_found)
+
+path = "files/"
+os.chdir(path)
+print(os.listdir("."))
+
 todo_list = {}
 counter = 1
+
 while True:
     user_choice = input("\nPress 1 for adding the task\nPress 2 for editing the task\nPress 3 for removing the task\nPress 4 for displaying the todo list\nPress 5 for exiting the program\n")
     if(user_choice == "1"):
@@ -23,11 +28,8 @@ while True:
         print("TODO List")
         print(todo_list)
     elif (user_choice == "5"):
-        file = open("todo_list.txt", "a")
-        file.write(name)
-        file.write(",")
-        file.write(str(todo_list))
-        file.write("\n")
+        file = open(f"files/{name}.json", "w")
+        file.write(json.dumps(todo_list))
         file.close()
-        print("Thank You for using the TODO List, Have a good day!")
+        print("Thank You for using the TODO App, Have a good day!")
         exit()
